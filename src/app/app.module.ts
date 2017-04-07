c538fdf816b8fa42ab3eb9c0d0f8947a545131ef
bcooper085 <bcooper085@gmail.com>
2017-04-07 09:34:03 -0700

    add header and links for home and about, complete jumbotron for home and about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { RosterComponent } from './roster/roster.component';
 import { routing } from './app.routing';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RosterComponent
+    RosterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
-
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RosterComponent } from './roster/roster.component';
+import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: RosterComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
   },
 ];
 
